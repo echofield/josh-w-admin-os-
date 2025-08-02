58cbe52a4d4c9d34aaca3f0a672e83cabf1108cf
echofield <echofield@outlook.fr>
2025-08-02 14:26:14 +0300

    Final deployment preparation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "lightningcss-loader": false,
+      "lightningcss-linux-x64-gnu": false,
+      "lightningcss-linux-x64-musl": false
+    };
+    return config;
+  }
+}
